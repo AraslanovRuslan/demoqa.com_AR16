d2b6da21b1d92ea851cdc04cb9dbf883fd7db615
r.araslanov <araslanov.08@gmail.com>
2022-11-14 11:43:04 +0500

    #userNumber").setValue("89000765432")

--- src/test/java/TestutomationTests.java
+++ src/test/java/TestutomationTests.java
@@ -2,6 +2,8 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -23,6 +25,19 @@ public class TestutomationTests {
         $("#userNumber").setValue("89000765432");
         $("[id=dateOfBirthInput]").click();
         $(".react-datepicker__month-select").click();
-        $(".react-datepicker__month-select").selectOption("January");
+        $(".react-datepicker__month-select").selectOption("May");
+        $(".react-datepicker__year-select").click();
+        $(".react-datepicker__year-select").selectOption("1990");
+        $(".react-datepicker__year-select").click();
+        $(".react-datepicker__day--026").click();
+        $("#subjectsInput").click();
+        $("#subjectsInput").setValue("Painting");
+        $("#subjectsInput").pressEnter();
+        $("#subjectsInput").pressTab();
+        $("#hobbies-checkbox-2").parent().click();
+        $("#uploadPicture").uploadFile(new File("src/test/resources1/paddington-g51aae444a_640.png"));
+
+
+
     }
 }
