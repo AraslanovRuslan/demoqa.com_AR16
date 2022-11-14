b6d6ca02bfb4e98a1dac98608876ab9ecd45b7f3
r.araslanov <araslanov.08@gmail.com>
2022-11-14 11:57:29 +0500

    #userNumber").setValue("89000765432")

--- src/test/java/TestutomationTests.java
+++ src/test/java/TestutomationTests.java
@@ -1,9 +1,12 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -36,8 +39,28 @@ public class TestutomationTests {
         $("#subjectsInput").pressTab();
         $("#hobbies-checkbox-2").parent().click();
         $("#uploadPicture").uploadFile(new File("src/test/resources1/paddington-g51aae444a_640.png"));
+        $("#currentAddress").setValue("Some Address");
+        $("#state").click();
+        $("#stateCity-wrapper").$(byText("NCR")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(byText("Delhi")).click();
+        $("#submit").click();
 
-
+        $(".modal-content").shouldBe(Condition.visible);
+        $(".modal-content").shouldHave(text("Mike Turilov"));
+        $(".modal-content").shouldHave(text("example@google.com"));
+        $(".modal-content").shouldHave(text("Male"));
+        $(".modal-content").shouldHave(text("9001546995"));
+        $(".modal-content").shouldHave(text("12 January,1985"));
+        $(".modal-content").shouldHave(text("Maths"));
+        $(".modal-content").shouldHave(text("Sports"));
+        $(".modal-content").shouldHave(text("1.png"));
+        $(".modal-content").shouldHave(text("Some Address"));
+        $(".modal-content").shouldHave(text("NCR Delhi"));
+        $("#closeLargeModal").click();
 
     }
+
+
+
 }
